f2619f1989ebf7658e30e200cdbc91da011db7f4
Cedan Misquith <cedanmisquith@gmail.com>
2020-04-02 13:54:24 +0530

    Initial Commit

---
+++ LottieLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieLoader
+//
+//  Created by Cedan Misquith on 02/04/20.
+//  Copyright © 2020 Cedan Misquith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
